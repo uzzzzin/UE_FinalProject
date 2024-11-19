2b8eb69e9f81a0c4ff7686ef377b77081de928a1
woo <woo060874@naver.com>
2024-11-20 01:44:19 +0900

    feat: FinalProject 전용 GameInstance 추가 및 기본 설정 변경
    - FinalProject에 맞춘 커스텀 GameInstance 클래스를 추가함.
    - 프로젝트의 기본 GameInstance를 새로 추가한 FinalProjectGameInstance로 변경함.

---
+++ FinalProject/Source/FinalProject/FinalProjectGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "FinalProjectGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJECT_API UFinalProjectGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
